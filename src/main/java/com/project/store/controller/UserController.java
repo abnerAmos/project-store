9e8b10cbe00769fe00d261dbe93eeded4dce0b99
abnerAmos <abner.fsouza1@gmail.com>
2023-02-02 11:15:19 -0300

    Inicio Projeto, Implementação Banco H2

--- src/main/java/com/project/store/controller/UserController.java
+++ src/main/java/com/project/store/controller/UserController.java
@@ -1,2 +1,15 @@
-package com.project.store.controller;public class UserController {
+package com.project.store.controller;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById (@PathVariable("id") Long id) {
+        return null;
+    }
 }
--- src/main/java/com/project/store/model/User.java
+++ src/main/java/com/project/store/model/User.java
@@ -1,2 +1,23 @@
-package com.project.store.model;public class User {
+package com.project.store.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
 }
